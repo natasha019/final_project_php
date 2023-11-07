103f09da1788462472a6d408f2b007b930150409
Natasha Ramos <natasha.ramos5@upr.edu>
2023-11-06 22:07:53 -0400

    updated courses

--- estudiantes/courses.php
+++ estudiantes/courses.php
@@ -54,75 +54,72 @@ if (!isset($_GET['desde'])) {
 
 
     <!-- courses section -->
-    <?php
-    include_once("../db_info.php");
-    $query = "SELECT COUNT(course_id) as contador
-                FROM course";
-
-    $result = $dbc->query($query);
-    $row = $result->fetch_assoc();
-    $contador = $row['contador'];
-    $total_pags = ceil($contador / $limite);
-    $pag_actual = ceil($desde / $limite) + 1;
-
-    $query = " SELECT * FROM course";
-
-    try {
-        if ($result = $dbc->query($query)) {
-            print " <table class='table table-striped'>";
-            print "<tr> 
-                        <th>Seccion</th>
-                        <th>Codigo</th>
-                        <th>Nombre del curso</th>
-                        <th>Creditos</th>
-                        <th>Cupo</th>
-                    </tr>";
-            while ($row = $result->fetch_assoc()) {
-
-                print "<tr>
-                            <td>" . $row['section_id'] . "</td>
-                            <td>" . $row['course_id'] . "</td>
-                            <td>" . $row['title'] . "</td>
-                            <td>" . $row['credits'] . "</td>
-                            <td>" . $row['capacity'] . "</td>
-                        </tr>";
-            }
-            print "</table>";
-            echo "<h2 style='text-align:center'>";
-
-            for ($i = 1; $i <= $total_pags; $i++)
-                echo "<a  class='btn pages' href='index.php?desde=" . (($i - 1) * $limite) . "&limite=$limite&ordenID=$ordenID'> $i </a>&nbsp;&nbsp;";
-
-            echo "</h2>";
-        }
-    } catch (Exception $e) {
-        print "<h3 style=\"color:red\">Error en el query: " . $dbc->error . "</h3>";
-    } finally {
-        $dbc->close();
-    }
-
-    ?>
-
-    <section class="service_section layout_padding">
+    <section class="service_section mt-3">
         <div class="service_container">
             <div class="container ">
                 <div class="heading_container heading_center">
-                    <h2>
+                    <h2 class="mb-5">
                         Cursos <span>Disponibles</span>
                     </h2>
                 </div>
                 <div>
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Seccion</th>
-                            <th>Hora</th>
-                            <th>Nombre del curso</th>
-                            <th>Cupo</th>
-                        </tr>
-                        <tr>
-                            <td></td>
-                        </tr>
-                    </table>
+                    <?php
+                    include_once("../db_info.php");
+                    $query = "SELECT COUNT(course_id) as contador
+                FROM course";
+
+                    $result = $dbc->query($query);
+                    $row = $result->fetch_assoc();
+                    $contador = $row['contador'];
+                    $total_pags = ceil($contador / $limite);
+                    $pag_actual = ceil($desde / $limite) + 1;
+
+                    $query = "SELECT course_id,title,section_id,credits,capacity FROM course NATURAL join section ORDER BY course_id,section_id";
+
+                    try {
+                        if ($result = $dbc->query($query)) {
+                            print   "<div class='row d-flex justify-content-end pr-3'>
+                            <div class='col-'><form class='d-flex'>
+                            <input class='form-control me-sm-2' type='search' placeholder='Search'>
+                            <button class='btn btn-secondary my-2 my-sm-0' type='submit'>Search</button>
+                          </form></div>
+                          </div>";
+
+                            print " <table class='table table-striped'>";
+                            print "<tr> 
+                        <th>Codigo</th>
+                        <th>Seccion</th>                        
+                        <th>Nombre del curso</th>
+                        <th>Creditos</th>
+                        <th>Cupo</th>
+                    </tr>";
+                            while ($row = $result->fetch_assoc()) {
+
+                                print "<tr>
+                            <td>" . $row['course_id'] . "</td>
+                            <td>" . $row['section_id'] . "</td>
+                            <td>" . $row['title'] . "</td>
+                            <td>" . $row['credits'] . "</td>
+                            <td>" . $row['capacity'] . "</td>
+                        </tr>";
+                            }
+                            print "</table>";
+                            echo "<h2 style='text-align:center'>";
+
+                            for ($i = 1; $i <= $total_pags; $i++)
+                                echo "<a  class='btn pages' href='courses.php?desde=" . (($i - 1) * $limite) . "&limite=$limite'> $i </a>&nbsp;&nbsp;";
+
+                            echo "</h2>";
+                        }
+                    } catch (Exception $e) {
+                        print "<h3 style=\"color:red\">Error en el query: " . $dbc->error . "</h3>";
+                    } finally {
+                        $dbc->close();
+                    }
+
+                    ?>
+
+
                 </div>
             </div>
     </section>
