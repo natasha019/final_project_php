aa0e80ff977a09986d77be2ee16b65dbc78a5a41
Gabriel Sanchez <gabriel.sanchez17@upr.edu>
2023-11-15 13:18:48 -0400

    minor fix

---
+++ admin/reporte_estudiantes.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+
+$limite = 5;
+
+if (!isset($_GET['desde'])) {
+
+    $desde = 0;
+} else {
+
+    $desde = $_GET['desde'];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="../assets/images/favicon.png" type="">
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/trash-empty.css' rel='stylesheet'>
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/add-r.css' rel='stylesheet'>
+
+    <title>Pre-Matricula</title>
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css" />
+
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+    <!--owl slider stylesheet -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <!-- font awesome style -->
+    <link href="../assets/css/font-awesome.min.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="../assets/css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body class="sub_page">
+
+    <!-- start navbar -->
+    <?php include("layouts/header.php"); ?>
+
+
+    <!-- courses section -->
+    <section class="service_section mt-3">
+        <div class="service_container">
+            <div class="container ">
+                <div class="heading_container heading_center">
+                    <h2 class="mb-5">
+                        Estudiantes <span>Matriculados</span>
+                    </h2>
+                </div>
+                <div>
+                    <?php
+                    include_once("../db_info.php");
+                    //query para insertar clases
+
+                    //query para ver las clases
+                    $query = "SELECT COUNT(course_id) as contador
+                     FROM course";
+
+                    $result = $dbc->query($query);
+                    $row = $result->fetch_assoc();
+                    $contador = $row['contador'];
+                    $total_pags = ceil($contador / $limite);
+                    $pag_actual = ceil($desde / $limite) + 1;
+
+                    $query = "SELECT distinct student_id, course_id, section_id
+                    FROM enrollment                  
+                    ORDER BY student_id";
+
+                    try {
+                        if ($result = $dbc->query($query)) {
+                            print   "<div class='row d-flex justify-content-end pr-3'>
+                            
+                            <div class='col-'><form class='d-flex'>
+                            <input class='form-control me-sm-2' type='search' placeholder='Search'>
+                            <button class='btn btn-secondary my-2 my-sm-0' type='submit'>Search</button>
+                          </form></div>
+                          </div>";
+
+                            print " <table class='table table-striped'>";
+                            print "<tr> 
+                            
+                          
+                        <th>Numero Estudiante</th>
+                        <th>Curso</th>
+                        <th>Seccion</th>
+                    </tr>";
+                            while ($row = $result->fetch_assoc()) {
+                                print "<tr><form method='POST'>
+
+                            <td>" . $row['student_id'] . "<input type='hidden' name='course_id' value='" . $row["course_id"] . "'></td>
+                            <td>" . $row['course_id'] . "</td>
+                            <td>" . $row['section_id'] . "</td>
+                        </form></tr>";
+                            }
+                            print "</table>";
+                            echo "<h2 style='text-align:center'>";
+
+                            for ($i = 1; $i <= $total_pags; $i++)
+                                echo "<a  class='btn pages' href='courses.php?desde=" . (($i - 1) * $limite) . "&limite=$limite'> $i </a>&nbsp;&nbsp;";
+
+                            echo "</h2>";
+                        }
+                    } catch (Exception $e) {
+                        print "<h3 style=\"color:red\">Error en el query: " . $dbc->error . "</h3>";
+                    } finally {
+                        $dbc->close();
+                    }
+
+                    ?>
+
+
+                </div>
+            </div>
+    </section>
+
+
+    <!-- end courses section -->
+
+    <!-- footer section -->
+
+    <?php include("layouts/footer.php") ?>
+
+    <!-- end footer section -->
+
+
+</body>
+
+</html>
