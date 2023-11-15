6c47185a174bbaf5182de3e0706c9b248226857a
Gabriel Sanchez <gabriel.sanchez17@upr.edu>
2023-11-15 16:21:27 -0400

    estados de reportes funcionales

--- admin/reporte_estudiantes.php
+++ admin/reporte_estudiantes.php
@@ -4,10 +4,8 @@ session_start();
 $limite = 5;
 
 if (!isset($_GET['desde'])) {
-
     $desde = 0;
 } else {
-
     $desde = $_GET['desde'];
 }
 ?>
@@ -37,7 +35,7 @@ if (!isset($_GET['desde'])) {
     <!-- fonts style -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
 
-    <!--owl slider stylesheet -->
+    <!-- owl slider stylesheet -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
 
     <!-- font awesome style -->
@@ -68,37 +66,51 @@ if (!isset($_GET['desde'])) {
                 <div>
                     <?php
                     include_once("../db_info.php");
-                    //query para insertar clases
 
-                    //query para ver las clases
-                    $query = "SELECT COUNT(course_id) as contador
-                     FROM course";
+                    // Default query
+                    $query = "SELECT distinct student_id, course_id, section_id FROM enrollment where status = 1 ORDER BY student_id";
 
-                    $result = $dbc->query($query);
-                    $row = $result->fetch_assoc();
-                    $contador = $row['contador'];
-                    $total_pags = ceil($contador / $limite);
-                    $pag_actual = ceil($desde / $limite) + 1;
+                    if (isset($_GET['queryType'])) {
+                        $queryType = $_GET['queryType'];
 
-                    $query = "SELECT distinct student_id, course_id, section_id
-                    FROM enrollment
-                    where status = 1            
-                    ORDER BY student_id";
+                        // Customize your queries based on the selected queryType
+                        if ($queryType === 'prematriculado') {
+                            $query = "SELECT distinct student_id, course_id, section_id FROM enrollment where status = 0 ORDER BY student_id";
+                        } elseif ($queryType === 'Matriculados') {
+                            $query = "SELECT distinct student_id, course_id, section_id FROM enrollment where status = 1 ORDER BY student_id";
+                        } elseif ($queryType === 'denegados') {
+                            $query = "SELECT distinct student_id, course_id, section_id FROM enrollment where status = 2 ORDER BY student_id";
+                        }
+                    }
 
                     try {
                         if ($result = $dbc->query($query)) {
                             print   "<div class='row d-flex justify-content-end pr-3'>
-                            
-                            <div class='col-'><form class='d-flex'>
-                            <input class='form-control me-sm-2' type='search' placeholder='Search'>
-                            <button class='btn btn-secondary my-2 my-sm-0' type='submit'>Search</button>
-                          </form></div>
-                          </div>";
+        
+                            <div class='col-'>
+                            </div>
+        
+                            <div class='col-auto'>
+                                <div class='dropdown'>
+                                    <button class='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton'
+                                        data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                                        Select Query
+                                    </button>
+                                    <div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+                                        <a class='dropdown-item' href='reporte_estudiantes.php?queryType=default'>Default</a>
+                                        <a class='dropdown-item' href='reporte_estudiantes.php?queryType=prematriculado'>Pre - Matriculados</a>
+                                        <a class='dropdown-item' href='reporte_estudiantes.php?queryType=Matriculados'>Matriculados</a>
+                                        <a class='dropdown-item' href='reporte_estudiantes.php?queryType=denegados'>Denegados</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        ";
 
                             print " <table class='table table-striped'>";
                             print "<tr> 
-                            
-                          
+      
+    
                         <th>Numero Estudiante</th>
                         <th>Curso</th>
                         <th>Seccion</th>
@@ -115,7 +127,7 @@ if (!isset($_GET['desde'])) {
                             echo "<h2 style='text-align:center'>";
 
                             for ($i = 1; $i <= $total_pags; $i++)
-                                echo "<a  class='btn pages' href='courses.php?desde=" . (($i - 1) * $limite) . "&limite=$limite'> $i </a>&nbsp;&nbsp;";
+                                echo "<a  class='btn pages' href='courses.php?limite=$limite'> $i </a>&nbsp;&nbsp;";
 
                             echo "</h2>";
                         }
@@ -124,23 +136,16 @@ if (!isset($_GET['desde'])) {
                     } finally {
                         $dbc->close();
                     }
-
                     ?>
-
-
                 </div>
             </div>
     </section>
 
-
     <!-- end courses section -->
 
     <!-- footer section -->
-
     <?php include("layouts/footer.php") ?>
-
     <!-- end footer section -->
-
 
 </body>
 
